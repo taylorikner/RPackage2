bca8a50afa696a40df2ba9b59ed16348f59a2bad
J Kyle Armstrong <j.kyle.armstrong@gmail.com>
2024-06-15 11:30:53 -0400

    DeckOfCards
    an R6 method to simulate a Deck of Cards and a Boot of Decks. It turns out you cannot make generics for R6 classes as
    
    "This code illustrates an important aspect of R6. Because methods are bound to individual objects, the previously created hadley object does not get this new method
    "
    
    as stated in
    
    https://adv-r.hadley.nz/r6.html#r6-important-methods

--- R/DeckOfCards.R
+++ R/DeckOfCards.R
@@ -19,16 +19,16 @@
 #' drawn_cards1 <- my_deck$draw(5)
 #' # Print drawn cards
 #' print(my_deck$drawn)
-#' should be the same
+#' # should be the same
 #' drawn_cards1 == my_deck$drawn
 #' # Draw more cards
 #' drawn_cards2 <- my_deck$draw(3)
-#' should be the same
+#' # should be the same
 #' rbind(drawn_cards1, drawn_cards2) == my_deck$drawn
 #' # Print drawn cards
 #' print(my_deck$drawn)
 #' # Check number of cards
-#'nrow(rbind(drawn_cards1, drawn_cards2)) + nrow(my_deck$deck) == 52
+#' nrow(rbind(drawn_cards1, drawn_cards2)) + nrow(my_deck$deck) == 52
 #' # Reshuffle the deck
 #' my_deck$reshuffle()
 #' nrow(my_deck$deck) == 52
@@ -36,7 +36,7 @@
 #' # draw 40 cards
 #' drawn_cards3 <- my_deck$draw(40)
 #' # sort the remaining cards
-#' my_deck$solitaire("deck")
+#' my_deck$solitaire("deck") |> print()
 #'
 
 DeckOfCards <- R6::R6Class("DeckOfCards",
@@ -90,26 +90,26 @@ DeckOfCards <- R6::R6Class("DeckOfCards",
                            self$deck <- rbind(self$deck, self$drawn)
                            self$deck <- self$deck[sample(nrow(self$deck)), ]
                            self$drawn <- data.frame()
-                         }
+                         },
 
                          #' @description
                          #' sort the deck of cards.
-                         #' @param hand_type
+                         #' @param hand
                          #' either the `deck` or the `drawn` hand.
                          #' @return a data frame representing the sorted deck of cards.
 
-                         solitaire = function(hand_type = 'deck') {
-                           if (hand_type != "deck" && hand_type != "drawn") {
+                         solitaire = function(hand = 'deck') {
+                           if (hand != "deck" && hand != "drawn") {
                              stop("Invalid hand type. Please specify 'deck' or 'drawn'.")
                            }
 
-                           hand <- if (hand_type == "deck") self$deck else self$drawn
-                           hand <- hand[order(hand$Suit, hand$Card), ]
+                           hand_held <- if (hand == "deck") self$deck else self$drawn
+                           hand_held <- hand_held[order(hand_held$Suit, hand_held$Card), ]
 
-                           if (hand_type == "deck") {
-                             self$deck <- hand
+                           if (hand == "deck") {
+                             self$deck <- hand_held
                            } else {
-                             self$drawn <- hand
+                             self$drawn <- hand_held
                            }
                          }
                        )
@@ -125,27 +125,29 @@ DeckOfCards <- R6::R6Class("DeckOfCards",
 #' @import R6
 #'
 #' @examples
+#' # Create a new boot of decks
+#' my_boot <- BootOfDecks$new(2)
 #' drawn_cards1 <- my_boot$draw(5)
 #' # Print drawn cards
-#' print(drawn_cards)
+#' print(drawn_cards1)
 #' # should be the same
 #' drawn_cards1 == my_boot$drawn
 #' # Draw more cards
 #' drawn_cards2 <- my_boot$draw(5)
 #' # should be the same
-#' bind_rows(drawn_cards1,drawn_cards2) == my_boot$drawn
+#' rbind(drawn_cards1,drawn_cards2) == my_boot$drawn
 #' # Print remaining cards
 #' print(my_boot$deck)
 #' # Reshuffle the boot
 #' my_boot$reshuffle()
-#' should be empty
+#' # should be empty
 #' print(my_boot$drawn)
 #'
 #'
 #' @export BootOfDecks
 BootOfDecks <- R6::R6Class("BootOfDecks",
                        public = list(
-                         #' @field decks
+                         #' @field deck
                          #' A list of DeckOfCards objects.
                          deck = data.frame(),
 
@@ -161,7 +163,7 @@ BootOfDecks <- R6::R6Class("BootOfDecks",
 
                          initialize = function(number_of_decks) {
                            for (i in 1:number_of_decks) {
-                             self$deck <- DeckOfCards$new()$deck
+                             self$deck <- rbind(DeckOfCards$new()$deck, self$deck)
                            }
                          },
 
@@ -190,68 +192,28 @@ BootOfDecks <- R6::R6Class("BootOfDecks",
                            self$deck <- self$deck[!rownames(self$deck) %in% rownames(drawn_cards), ]
                            self$drawn <-  rbind(self$drawn, drawn_cards)
                            return(drawn_cards)
-                         }
+                         },
 
                          #' @description
                          #' sort the deck of cards.
-                         #' @param hand_type
+                         #' @param hand
                          #' either the `deck` or the `drawn` hand.
                          #' @return a data frame representing the sorted deck of cards.
 
-                         solitaire = function(hand_type = 'deck') {
-                           if (hand_type != "deck" && hand_type != "drawn") {
+                         solitaire = function(hand = 'deck') {
+                           if (hand != "deck" && hand != "drawn") {
                              stop("Invalid hand type. Please specify 'deck' or 'drawn'.")
                            }
 
-                           hand <- if (hand_type == "deck") self$deck else self$drawn
-                           hand <- hand[order(hand$Suit, hand$Card), ]
+                           hand_held <- if (hand == "deck") self$deck else self$drawn
+                           hand_held <- hand_held[order(hand_held$Suit, hand_held$Card), ]
 
-                           if (hand_type == "deck") {
-                             self$deck <- hand
+                           if (hand == "deck") {
+                             self$deck <- hand_held
                            } else {
-                             self$drawn <- hand
+                             self$drawn <- hand_held
                            }
                          }
                        )
 )
 
-#' Draw cards from a deck
-#'
-#' @description
-#' This function draws a specified number of cards from a deck.
-#'
-#' @param deck The deck object (either DeckOfCards or BootOfDecks).
-#' @param draws The number of cards to draw.
-#' @return A data frame representing the drawn cards.
-#' @examples
-#' my_deck <- DeckOfCards$new()
-#' my_deck$draw(5)
-#' my_boot <- BootOfDecks$new(2)
-#' my_boot$draw(5)
-#'
-#' @export draw_cards
-draw_cards <- function(deck, draws = 1, hand = "deck") {
-  # Validate the 'hand' parameter
-  if (!hand %in% c("deck", "drawn")) {
-    stop("Invalid hand type. Please specify 'deck' or 'drawn'.")
-  }
-
-  # Select the appropriate hand
-  selected_hand <- if (hand == "deck") deck$deck else deck$drawn
-
-  if (nrow(selected_hand) < draws) {
-    stop("Not enough cards left in the ", hand, " to draw the requested number of cards.")
-  }
-
-  drawn_cards <- selected_hand[sample(nrow(selected_hand), draws), ]
-
-  # Update the appropriate hand
-  if (hand == "deck") {
-    deck$deck <- deck$deck[!rownames(deck$deck) %in% rownames(drawn_cards), ]
-    deck$drawn <- rbind(deck$drawn, drawn_cards)
-  } else {
-    deck$drawn <- deck$drawn[!rownames(deck$drawn) %in% rownames(drawn_cards), ]
-  }
-
-  return(drawn_cards)
-}
