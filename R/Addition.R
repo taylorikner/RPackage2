3dfcb25ca2555b33eb1d9513871404541a252329
J Kyle Armstrong <j.kyle.armstrong@gmail.com>
2024-06-15 01:28:48 -0400

    Update Addition.R

--- R/Addition.R
+++ R/Addition.R
@@ -6,7 +6,10 @@
 #' @return Sum of x and y
 #'
 #' @examples
+#'
 #' addition(3, 5)
+#' addition(-4, 6)
+#' addition(exp(pi*complex(1,0,1)), 1) # close enough to 0
 #'
 #' @export addition
 addition <- function(x, y) {
